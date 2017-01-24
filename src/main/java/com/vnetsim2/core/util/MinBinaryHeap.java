77f4877c5f105c0490accbb1c2811575adf118c6
Vu Thanh Cong <vuthanhcong.ict@gmail.com>
2017-01-24 22:47:03 +0700

    Add implementation of min-binary-heap

--- src/main/java/com/vnetsim2/core/util/MinBinaryHeap.java
+++ src/main/java/com/vnetsim2/core/util/MinBinaryHeap.java
@@ -1,10 +1,9 @@
 package com.vnetsim2.core.util;
 
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class MinBinaryHeap<Key> implements Iterable<Key> {
+public class MinBinaryHeap<Key> {
     private Key[] keys;
     private int size;
     private Comparator<Key> comparator;
@@ -28,6 +27,19 @@ public class MinBinaryHeap<Key> implements Iterable<Key> {
         this(1, comparator);
     }
 
+    public MinBinaryHeap(Key[] keys, Comparator<Key> comparator) {
+        size = keys.length;
+        this.comparator = comparator;
+        this.keys = (Key[]) new Object[keys.length + 1];
+        for (int i = 0; i < size; i++) {
+            this.keys[i+1] = keys[i]; 
+        }
+        int lastInnerNode = size/2;
+        for (int j = lastInnerNode; j > 0; j++) {
+            sink(j);
+        }
+    }
+
     public boolean isEmpty() {
         return size == 0;
     }
@@ -43,8 +55,37 @@ public class MinBinaryHeap<Key> implements Iterable<Key> {
         return keys[1];
     }
 
+    public void insert(Key k) {
+        if (size == keys.length - 1) {
+            resize(2 * keys.length);
+        }
+        keys[++size] = k;
+        swim(size);
+    }
+
+    public Key extractMin() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Heap underflow");
+        }
+        exchange(1, size);
+        Key min = keys[size--];
+        sink(1);
+        keys[size+1] = null;
+        if ((size > 0) && (size == (keys.length - 1) / 4)) {
+            resize(keys.length / 2);
+        }
+        return min;
+    }
+
+    public MinBinaryHeap<Key> clone() {
+        MinBinaryHeap<Key> copy = new MinBinaryHeap<>(size, comparator);
+        for (int i = 1; i <= size; i++) {
+            copy.insert(keys[i]);
+        }
+        return copy;
+    }
+
     private void resize(int capacity) {
-        assert capacity > size;
         Key[] temp = (Key[]) new Object[capacity];
         for (int i = 1; i <= size; i++) {
             temp[i] = keys[i];
@@ -79,10 +120,10 @@ public class MinBinaryHeap<Key> implements Iterable<Key> {
             int left = k*2;
             int right = left + 1;
             int smaller = left;
-            if (right <= size && greater(left, right) {
+            if (right <= size && greater(left, right)) {
                 smaller = right;
             }
-            if (!greater(k, smaller) {
+            if (!greater(k, smaller)) {
                 break;
             }
             exchange(k, smaller);
