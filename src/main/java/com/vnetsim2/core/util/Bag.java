1f864e4eeb3ead0f6e3b25d7cfe0bebd6c7794b5
Vu Thanh Cong <vuthanhcong.ict@gmail.com>
2017-02-21 23:14:02 +0700

    Add javadoc for Bag

--- src/main/java/com/vnetsim2/core/util/Bag.java
+++ src/main/java/com/vnetsim2/core/util/Bag.java
@@ -3,6 +3,11 @@ package com.vnetsim2.core.util;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Represents a bag of generic items.
+ *
+ * @param <ItemT> the generic type of an item.
+ */
 public class Bag<ItemT> implements Iterable<ItemT> {
   private Node<ItemT> head;
   private int size;
@@ -12,19 +17,41 @@ public class Bag<ItemT> implements Iterable<ItemT> {
     private Node<ItemT> next;
   }
 
+  /**
+   * Initialize an empty bag.
+   */
   public Bag() {
     head = null;
     size = 0;
   }
 
+  /**
+   * Returns true if this bag is empty.
+   * O(1)
+   *
+   * @return {@code true} if this bag is empty,
+   *         {@code false} otherwise.
+   */
   public boolean isEmpty() {
     return head == null;
   }
 
+  /**
+   * Returns the number of items in this bag.
+   * O(1)
+   *
+   * @return the number of items in this bag.
+   */
   public int size() {
     return size;
   }
 
+  /**
+   * Adds a item to this bag.
+   * O(1)
+   *
+   * @param item the item to add.
+   */
   public void add(ItemT item) {
     Node<ItemT> oldHead = head;
     head = new Node<>();
@@ -33,6 +60,12 @@ public class Bag<ItemT> implements Iterable<ItemT> {
     size++;
   }
 
+  /**
+   * Returns an iterator that iterates over the items in this bag in arbitrary order.
+   *
+   * @return an iterator.
+   */
+  @Override
   public Iterator<ItemT> iterator() {
     return new ListIterator<ItemT>(head);
   }
@@ -44,14 +77,17 @@ public class Bag<ItemT> implements Iterable<ItemT> {
       current = head;
     }
 
+    @Override
     public boolean hasNext() {
       return current != null;
     }
 
+    @Override
     public void remove() {
       throw new UnsupportedOperationException();
     }
 
+    @Override
     public ItemT next() {
       if (!hasNext()) {
         throw new NoSuchElementException();
