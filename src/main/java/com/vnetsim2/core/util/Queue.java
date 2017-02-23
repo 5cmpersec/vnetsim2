37dfe9a855553b15d954f9dfc3239e82d21738f8
Vu Thanh Cong <vuthanhcong.ict@gmail.com>
2017-02-23 21:45:35 +0700

    Add javadoc for Queue

--- src/main/java/com/vnetsim2/core/util/Queue.java
+++ src/main/java/com/vnetsim2/core/util/Queue.java
@@ -2,6 +2,11 @@ package com.vnetsim2.core.util;
 
 import java.util.NoSuchElementException;
 
+/**
+ * Represents a FIFO queue of generic items.
+ *
+ * @param <ItemT> the generic type of an item.
+ */
 public class Queue<ItemT> {
   private Node<ItemT> head;
   private Node<ItemT> tail;
@@ -12,20 +17,42 @@ public class Queue<ItemT> {
     private Node<ItemT> next;
   }
 
+  /**
+   * Initialize an empty queue.
+   */
   public Queue() {
     head = null;
     tail = null;
     size = 0;
   }
 
+  /**
+   * Returns true if this queue is empty.
+   * O(1)
+   *
+   * @return {@code true} if this queue is empty,
+   *         {@code false} otherwise.
+   */
   public boolean isEmpty() {
     return head == null;
   }
 
+  /**
+   * Returns the number of items in this queue.
+   * O(1)
+   *
+   * @return the number of items in this queue.
+   */
   public int size() {
     return size;
   }
 
+  /**
+   * Adds an item to this queue.
+   * O(1)
+   *
+   * @param item the item to add.
+   */
   public void enqueue(ItemT item) {
     Node<ItemT> oldTail = tail;
     tail = new Node<>();
@@ -39,6 +66,11 @@ public class Queue<ItemT> {
     size++;
   }
 
+  /**
+   * Removes and returns an item on this queue that was least recently added.
+   *
+   * @return an item on this queue that was least recently added.
+   */
   public ItemT dequeue() {
     if (isEmpty()) {
       throw new NoSuchElementException("Queue underflow");
