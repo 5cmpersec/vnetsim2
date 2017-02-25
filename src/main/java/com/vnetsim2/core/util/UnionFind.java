fa39ef8c8be6fe47bd704b08da62c8b0241c38e7
Vu Thanh Cong <vuthanhcong.ict@gmail.com>
2017-02-25 21:42:19 +0700

    Add javadoc and fix checkstyle warning UnionFind class.

--- src/main/java/com/vnetsim2/core/util/UnionFind.java
+++ src/main/java/com/vnetsim2/core/util/UnionFind.java
@@ -1,43 +1,90 @@
 package com.vnetsim2.core.util;
 
+/**
+ * Represents a union-find (disjoint-sets) data type.
+ *
+ */
 public class UnionFind {
   private int[] parent;
   private byte[] rank;
   private int count;
 
-  public UnionFind(int n) {
-    if (n < 0) {
+  /**
+   * Initializes an empty union–find data structure with n sites
+   * 0 through n-1.
+   * TimeComplexity: O(n) with n = number.
+   *
+   * @param  number the number of sites
+   * @throws IllegalArgumentException if number < 0.
+   */
+  public UnionFind(int number) {
+    if (number < 0) {
       throw new IllegalArgumentException("Number of sites must be nonnegative");
     }
-    count = n;
-    parent = new int[n];
-    rank = new byte[n];
-    for (int i = 0; i < n; i++) {
+    count = number;
+    parent = new int[number];
+    rank = new byte[number];
+    for (int i = 0; i < number; i++) {
       parent[i] = i;
       rank[i] = 0;
     }
   }
 
-  public int find(int p) {
-    validate(p);
-    while (p != parent[p]) {
-      parent[p] = parent[parent[p]];
-      p = parent[p];
+  /**
+   * Returns the component identifier for the component containing site p.
+   * TimeComplexity: nearly but not O(1)
+   *
+   * @param  pos the integer representing one site
+   * @return the component identifier for the component containing site p
+   * @throws IndexOutOfBoundsException unless 0 <= p < n.
+   */
+  public int find(int pos) {
+    validate(pos);
+    while (pos != parent[pos]) {
+      parent[pos] = parent[parent[pos]];
+      pos = parent[pos];
     }
-    return p;
+    return pos;
   }
 
+  /**
+   * Returns the number of components.
+   * TimeComplexity: O(1)
+   *
+   * @return the number of components (between 1 and n).
+   */
   public int count() {
     return count;
   }
 
-  public boolean connected(int p, int q) {
-    return find(p) == find(q);
+  /**
+   * Returns true if the the two sites are in the same component.
+   * TimeComplexity: nearly but not O(1)
+   *
+   * @param  pos1 the integer representing one site
+   * @param  pos2 the integer representing the other site
+   * @return {@code true} if the two sites p and q are in the same component;
+   *         {@code false} otherwise
+   * @throws IndexOutOfBoundsException unless
+   *         both 0 <= pos1 < n and 0 <= pos2 < n
+   */
+  public boolean connected(int pos1, int pos2) {
+    return find(pos1) == find(pos2);
   }
 
-  public void union(int p, int q) {
-    int rootP = find(p);
-    int rootQ = find(q);
+  /**
+   * Merges the component containing site p with the
+   * the component containing site q.
+   * TimeComplexity: nearly but not O(1)
+   *
+   * @param  pos1 the integer representing one site
+   * @param  pos2 the integer representing the other site
+   * @throws IndexOutOfBoundsException unless
+   *         both 0 <= pos1 < n and 0 <= pos2 < n
+   */
+  public void union(int pos1, int pos2) {
+    int rootP = find(pos1);
+    int rootQ = find(pos2);
 
     if (rootP == rootQ) {
       return;
@@ -55,9 +102,9 @@ public class UnionFind {
   }
 
   private void validate(int p) {
-    int n = parent.length;
-    if (p < 0 || p >= n) {
-      throw new IndexOutOfBoundsException("Index " + p + " is not between 0 and " + (n - 1));
+    int len = parent.length;
+    if (p < 0 || p >= len) {
+      throw new IndexOutOfBoundsException("Index " + p + " is not between 0 and " + (len - 1));
     }
   }
 }
