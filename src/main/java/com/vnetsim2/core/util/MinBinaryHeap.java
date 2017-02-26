b37563d96a5f05acde4f488632095733090ae453
Vu Thanh Cong <vuthanhcong.ict@gmail.com>
2017-02-26 22:45:50 +0700

    Add javadoc for MinBinaryHeap

--- src/main/java/com/vnetsim2/core/util/MinBinaryHeap.java
+++ src/main/java/com/vnetsim2/core/util/MinBinaryHeap.java
@@ -3,21 +3,41 @@ package com.vnetsim2.core.util;
 import java.util.Comparator;
 import java.util.NoSuchElementException;
 
+/**
+ * Represents a binary heap of generic keys.
+ *
+ * @param <KeyT> the generic type of key on this binary heap.
+ */
 public class MinBinaryHeap<KeyT> {
   private KeyT[] keys;
   private int size;
   private Comparator<KeyT> comparator;
 
+  /**
+   * Initializes an empty binary heap with the given capacity.
+   *
+   * @param initCapacity the initial capacity of this binary heap.
+   */
   @SuppressWarnings("unchecked")
   public MinBinaryHeap(int initCapacity) {
     keys = (KeyT[]) new Object[initCapacity + 1];
     size = 0;
   }
 
+  /**
+   * Initializes an empty binary heap.
+   */
   public MinBinaryHeap() {
     this(1);
   }
 
+  /**
+   * Initializes an empty binary heap with the given capacity,
+   * using the given comparator.
+   *
+   * @param initCapacity the initial capacity of this binary heap.
+   * @param comparator the comparator to use when comparing keys.
+   */
   @SuppressWarnings("unchecked")
   public MinBinaryHeap(int initCapacity, Comparator<KeyT> comparator) {
     this.comparator = comparator;
@@ -25,10 +45,22 @@ public class MinBinaryHeap<KeyT> {
     size = 0;
   }
 
+  /**
+   * Initializes an empty binary heap using the given comparator.
+   *
+   * @param comparator the comparator to use when comparing keys.
+   */
   public MinBinaryHeap(Comparator<KeyT> comparator) {
     this(1, comparator);
   }
 
+  /**
+   * Initializes an empty binary heap from the array of keys,
+   * using the given comparator.
+   *
+   * @param keys the array of keys.
+   * @param comparator the comparator to use when comparing keys.
+   */
   @SuppressWarnings("unchecked")
   public MinBinaryHeap(KeyT[] keys, Comparator<KeyT> comparator) {
     size = keys.length;
@@ -43,14 +75,35 @@ public class MinBinaryHeap<KeyT> {
     }
   }
 
+
+  /**
+   * Returns true if this binary heap is empty.
+   * TC: O(1)
+   *
+   * @return {@code true} if this binary heap is empty;
+   *         {@code false} otherwise
+   */
   public boolean isEmpty() {
     return size == 0;
   }
 
+  /**
+   * Returns the number of keys on this binary heap.
+   * TC: O(1)
+   *
+   * @return the number of keys on this binary heap.
+   */
   public int size() {
     return size;
   }
 
+  /**
+   * Returns a smallest key on this binary heap.
+   * TC: O(1)
+   *
+   * @return a smallest key on this binary heap.
+   * @throws NoSuchElementException if this binary heap is empty
+   */
   public KeyT min() {
     if (isEmpty()) {
       throw new NoSuchElementException("Heap underflow");
@@ -58,6 +111,12 @@ public class MinBinaryHeap<KeyT> {
     return keys[1];
   }
 
+  /**
+   * Adds a new key to this binary heap.
+   * TC: O(lgn)
+   *
+   * @param key the key to add to this binary heap
+   */
   public void insert(KeyT key) {
     if (size == keys.length - 1) {
       resize(2 * keys.length);
@@ -66,20 +125,28 @@ public class MinBinaryHeap<KeyT> {
     swim(size);
   }
 
+  /**
+   * Removes and returns a smallest key on this binary heap.
+   * TC: O(lgn)
+   *
+   * @return a smallest key on this binary heap.
+   * @throws NoSuchElementException if this binary heap is empty.
+   */
   public KeyT extractMin() {
     if (isEmpty()) {
       throw new NoSuchElementException("Heap underflow");
     }
     exchange(1, size);
-    KeyT min = keys[size--];
+    KeyT temp = keys[size--];
     sink(1);
     keys[size + 1] = null;
     if ((size > 0) && (size == (keys.length - 1) / 4)) {
       resize(keys.length / 2);
     }
-    return min;
+    return temp;
   }
 
+  @Override
   public MinBinaryHeap<KeyT> clone() {
     MinBinaryHeap<KeyT> copy = new MinBinaryHeap<>(size, comparator);
     for (int i = 1; i <= size; i++) {
